e415c3885f50e963c9b8d559fdbe6b47c75e49df
Alysson Barrera <alyssonbarrera@outlook.com>
2025-07-08 09:09:32 -0300

    ✨ add create question, create room and get room questions

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -3,6 +3,7 @@ import { db, sql } from './connection.ts'
 import { schema } from './schema/index.ts'
 
 await reset(db, schema)
+
 await seed(db, schema).refine((faker) => {
   return {
     rooms: {
@@ -11,9 +12,13 @@ await seed(db, schema).refine((faker) => {
         name: faker.companyName(),
         description: faker.loremIpsum(),
       },
+      with: {
+        questions: 5,
+      },
     },
   }
 })
+
 await sql.end()
 
 // biome-ignore lint/suspicious/noConsole: only used in dev
